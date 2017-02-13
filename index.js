2dff8d71696ed24a4e3216f0a448f29870a6dea1
Samuel Rounce <me@samuelrounce.co.uk>
2017-02-13 11:12:40 +0000

    Copied eslint rules from frontend

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = {
+    extends: 'airbnb',
+    ecmaVersion: 6,
+    sourceType: 'module',
+    parser: 'babel-eslint',
+    ecmaFeatures: {
+        jsx: true
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true
+        }
+    },
+    plugins: [
+        'react'
+    ],
+    rules: {
+        indent: ['error', 4],
+        'comma-dangle': ['error', 'never'],
+        'padded-blocks': ['error', 'never'],
+        'max-len': [0, 200],
+        'react/jsx-indent': [2, 4],
+        'react/jsx-indent-props': [2, 4],
+        camelcase: [0, { properties: 'never' }]
+    },
+    env: {
+        browser: true,
+        mocha: true,
+        node: true
+    }
+};
